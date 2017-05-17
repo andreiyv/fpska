e4552a90923055d6d3d7d3ac29978628ab598501
AndreyV <voran@inbox.ru>
2017-05-17 14:46:39 +0300

    First commit

---
+++ go.bat
@@ -0,0 +1,3 @@
+rem mencoder.exe sample.avs -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=9000000:mbd=2:trell:v4mv:last_pred=3:predia=2:dia=2:vmax_b_frames=2:vb_strategy=1:precmp=2:cmp=2:subcmp=2:preme=2:keyint=30 -o CAM01576_60fps_mencoder.mkv
+#ffmpeg -i 50_60fps.avs -c:v libx264 -preset slow -crf 15 -filter:v hqdn3d=5.0 -c:a copy 50_60fps.avi
+ffmpeg -i 50_60fps.avs -c:v libx264 -preset slow -crf 15 -c:a copy 50_60fps.avi
