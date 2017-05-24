c0914ee89eb51ad1018fcf10a1711fe2a4cc4a11
AndreyV <voran.mail@gmail.com>
2017-05-24 15:14:56 +0300

    update

--- fpska.bat
+++ fpska.bat
@@ -6,13 +6,16 @@ echo %time%
 copy %cd%\scripts\fpska.avs %cd%\scripts\work.avs
 
 set "search=fullhd.mkv"
+set "search_threads=nthreads"
 set "replace=%1"
+set "threads=%2"
 set "textfile=%cd%\scripts\work.avs"
 set "newfile=%cd%\scripts\tmp.txt"
 
 (for /f "delims=" %%i in (%textfile%) do (
     set "line=%%i"
     set "line=!line:%search%=%replace%!"
+    set "line=!line:%search_threads%=%threads%!"
     echo(!line!
 ))>"%newfile%"
 del %cd%\scripts\work.avs
@@ -25,8 +28,8 @@ endlocal
 
 
 
-del %cd%\scripts\work.avs
-del *.ffindex
+rem del %cd%\scripts\work.avs
+rem del *.ffindex
 
 
 echo %time%
