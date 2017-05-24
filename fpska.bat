24c631ff0cdcf83da76ec8e2f8afe6a7f5af3bf8
AndreyV <voran.mail@gmail.com>
2017-05-24 11:32:12 +0300

    update

--- fpska.bat
+++ fpska.bat
@@ -1,1 +1,34 @@
-%cd%/x264/x264-win32.exe --preset veryslow --output "60fps.mkv" "%cd%\scripts\fpska.avs"
+@echo off &setlocal
+setlocal enabledelayedexpansion
+
+echo %time%
+
+copy %cd%\scripts\fpska.avs %cd%\scripts\work.avs
+
+set "search=fullhd.mkv"
+set "replace=%1"
+set "textfile=%cd%\scripts\work.avs"
+set "newfile=%cd%\scripts\tmp.txt"
+
+(for /f "delims=" %%i in (%textfile%) do (
+    set "line=%%i"
+    set "line=!line:%search%=%replace%!"
+    echo(!line!
+))>"%newfile%"
+del %cd%\scripts\work.avs
+ren %cd%\scripts\tmp.txt work.avs
+endlocal
+
+@echo on
+
+%cd%/x264/x264-win32.exe --preset veryslow --output "60fps.mkv" "%cd%\scripts\work.avs"
+
+
+
+rem del %cd%\scripts\work.avs
+rem del *.ffindex
+
+
+echo %time%
+
+
--- fpska_no_GPU.bat
+++
@@ -1,1 +0,0 @@
-%cd%/x264/x264-win32.exe --preset veryslow --output "60fps.mkv" "%cd%\scripts\fpska_no_GPU.avs"
