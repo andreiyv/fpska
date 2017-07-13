5403686ddcfc35a202092a88bac427029f49b503
voran <voran@inbox.ru>
2017-07-13 19:44:11 +0300

    extract audio from video and merge them later

--- fpska.bat
+++ fpska.bat
@@ -3,6 +3,9 @@ setlocal enabledelayedexpansion
 
 echo %time%
 
+rem extract audio
+%cd%\mencoder\mplayer.exe -vc dummy -vo null -ao pcm:file=audio.wav,fast %1
+
 copy %cd%\scripts\fpska.avs %cd%\scripts\work.avs
 
 set "search=fullhd.mkv"
@@ -25,11 +28,15 @@ endlocal
 @echo on
 
 rem %cd%/x264/x264-win32.exe --preset veryslow --output "60fps.mkv" "%cd%\scripts\work.avs"
-%cd%\mencoder\mencoder.exe "scripts\work.avs" -oac copy -ovc x264 -x264encopts preset=veryslow -o "60fps.mkv"
+%cd%\mencoder\mencoder.exe "%cd%\scripts\work.avs" -oac copy -ovc x264 -x264encopts preset=veryslow -o "60fps.mp4"
 rem ffmpeg\ffmpeg.exe  -i "%cd%\scripts\work.avs"  -c:v libx264  -x264-params preset=slow  -y video.mkv
 
 rem del %cd%\scripts\work.avs
 rem del *.ffindex
+
+%cd%\mencoder\mencoder.exe -audiofile audio.wav 60fps.mp4 -o 60fps_video_and_audio.mp4 -ovc copy -oac copy
+
+
 
 echo %time%
 
