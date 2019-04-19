9d6082b15245c98825feb11aa8087683fedb23b6
voran <voran@inbox.ru>
2019-04-19 11:19:40 +0300

    chcp1251 and Russian resources

--- fpska.bat
+++ fpska.bat
@@ -1,10 +1,17 @@
+chcp 1251
 @echo off &setlocal
 setlocal enabledelayedexpansion
 
 cls
 
-CALL :Info_Message "fpska v0.5"
-CALL :Check_Install
+(set \n=^
+%=Do not remove this line=%
+)
+
+rem echo Line1!\n!Line2
+
+CALL :Info_Message "fpska v0.5 - скрипт для конвертации в 50/60 FPS"
+
 rem ============= init =============================
 set fpska_home=%~dp0
 set ffmpeg_threads=1
@@ -16,7 +23,6 @@ set video_file=%~f2
 set video_ext=%~x2
 rem =================================================
 
-
 FOR %%i IN ("%~f1") DO (
 rem ECHO filedrive=%%~di
 rem ECHO filepath=%%~pi
@@ -24,58 +30,10 @@ set video_file_name=%%~ni
 rem ECHO fileextension=%%~xi
 )
 
-echo Fpska home: !fpska_home! 
-echo File:  !video_file! 
-echo Extension:  !video_ext!
-
-
-if [!video_file!]==[] (
-echo Vvedite imya video
-pause
-exit
-)
-
-rem ============= get info =========================
-
-"!fpska_home!\ffmpeg\ffprobe.exe" -i "!video_file!" > "!fpska_home!\log.txt" 2> "!fpska_home!\ffprobe.log"
-
-findstr /m /c:"Audio: aac" "!fpska_home!ffprobe.log"
-if %errorlevel%==0 (
-	set audio_codeck=aac
-)
-
-findstr /m /c:"Audio: mp3" "!fpska_home!ffprobe.log"
-if %errorlevel%==0 (
-	set audio_codeck=mp3
-)
-findstr /m "matroska" "!fpska_home!ffprobe.log"
-if %errorlevel%==0 (
-	set container=mkv
-)
-
-findstr /m /c:"Video: h264" "!fpska_home!ffprobe.log"
-if %errorlevel%==0 (
-	set container=mp4
-)
-
-findstr /m /c:"mov,mp4,m4a,3gp,3g2,mj2" "!fpska_home!ffprobe.log"
-if %errorlevel%==0 (
-	set container=mp4
-)
-
-findstr /m /c:"mpegts" "!fpska_home!ffprobe.log"
-if %errorlevel%==0 (
-	set container=mpegts
-)
-
-findstr /m /c:"avi," "!fpska_home!ffprobe.log"
-if %errorlevel%==0 (
-	set container=avi
-)
-
-echo container: !container!
-echo audio: !audio_codeck!
-rem =================================================
+echo Fpska домашняя папка: !fpska_home!
+echo.
+echo Полный путь к файлу:  !video_file! 
+echo.
 
 rem ===================== set nethod ================
 if [%1]==[] (
@@ -90,9 +48,73 @@ set ncpu=4
 set ncpu=%3
 )
 
-echo Method: !method!
+echo Метод конвертации в 50/60fps: !method!
 
 rem =================================================
+
+
+if [!video_file!]==[] (
+echo Вы забыли указать имя файла
+echo.
+pause
+exit
+)
+
+CALL :Check_Install
+
+rem ============= get info =========================
+echo --------------------------------------------------------
+echo Извлекаем информацию о видео и аудио кодеках из видеофайла
+"!fpska_home!\ffmpeg\ffprobe.exe" -i "!video_file!" 1> NUL 2> "!fpska_home!\ffprobe.log"
+if %errorlevel%==0 (
+	echo Информация извлечена успешно в файл "!fpska_home!\ffprobe.log"
+	echo.
+) else (
+	echo Ошибка извлечения информации
+	pause
+	exit
+)
+
+findstr /m /c:"Audio: aac" "!fpska_home!ffprobe.log" >NUL
+if %errorlevel%==0 (
+	set audio_codeck=aac
+)
+
+findstr /m /c:"Audio: mp3" "!fpska_home!ffprobe.log" >NUL
+if %errorlevel%==0 (
+	set audio_codeck=mp3
+)
+findstr /m "matroska" "!fpska_home!ffprobe.log" >NUL
+if %errorlevel%==0 (
+	set container=mkv
+)
+
+findstr /m /c:"Video: h264" "!fpska_home!ffprobe.log" >NUL
+if %errorlevel%==0 (
+	set container=mp4
+)
+
+findstr /m /c:"mov,mp4,m4a,3gp,3g2,mj2" "!fpska_home!ffprobe.log" >NUL
+if %errorlevel%==0 (
+	set container=mp4
+)
+
+findstr /m /c:"mpegts" "!fpska_home!ffprobe.log" >NUL
+if %errorlevel%==0 (
+	set container=mpegts
+)
+
+findstr /m /c:"avi," "!fpska_home!ffprobe.log" >NUL
+if %errorlevel%==0 (
+	set container=avi
+)
+
+echo Информация о видеофайле:
+echo Контейнер исходного видеофайла: !container!
+echo Звуковая дорожка в формате: !audio_codeck!
+echo.
+echo --------------------------------------------------------
+
 
 rmdir /S/Q "!fpska_home!tmp"
 mkdir "!fpska_home!tmp"
@@ -104,28 +126,24 @@ CALL :Info_Message "script started at"
 echo %time%
 
 rem ============== extract audio ====================
+echo "Извлекаем звуковую дорожку из исходного видеофайла"
 if "!container!"=="mp4" (
- CALL :Info_Message "Extracting audio from mp4 container using ffmpeg"
  if "!audio_codeck!"=="aac" ( 
-"!fpska_home!ffmpeg\ffmpeg.exe" -y -i !video_file! -vn -acodec copy "!fpska_home!\tmp\60fps_audio.aac" -v quiet -stats 
+"!fpska_home!ffmpeg\ffmpeg.exe" -y -i !video_file! -vn -acodec copy "!fpska_home!\tmp\60fps_audio.aac" -v quiet
 )
 )
 
 if "!container!"=="avi" (
- CALL :Info_Message "Extracting audio from avi container using ffmpeg"
  if "!audio_codeck!"=="mp3" ( 
-"!fpska_home!ffmpeg\ffmpeg.exe" -y -i !video_file! -vn -acodec copy "!fpska_home!\tmp\60fps_audio.mp3" -v quiet -stats 
+"!fpska_home!ffmpeg\ffmpeg.exe" -y -i !video_file! -vn -acodec copy "!fpska_home!\tmp\60fps_audio.mp3" -v quiet
 )
 )
 
 if "!container!"=="mkv" (
-
- CALL :Info_Message "Extracting audio from mkv container using eac3to"
 copy "!video_file!" "!fpska_home!\tmp"
 cd "!fpska_home!\tmp"
 
 "!fpska_home!eac3to\eac3to.exe" "!fpska_home!\tmp\!video_file_name!!video_ext!" -demux
-
 del "!fpska_home!\tmp\!video_file_name!!video_ext!" 
 del "!fpska_home!\tmp\*.txt"
 del "!fpska_home!\tmp\*.h264"
@@ -134,16 +152,12 @@ del "!fpska_home!\tmp\*.vc1"
 cd "!fpska_home!"
 
 )
-
 
 if "!container!"=="mpegts" (
-
- CALL :Info_Message "Extracting audio from mpegts container using eac3to"
 copy "!video_file!" "!fpska_home!\tmp"
 cd "!fpska_home!\tmp"
 
 "!fpska_home!eac3to\eac3to.exe" "!fpska_home!\tmp\!video_file_name!!video_ext!" -demux
-
 del "!fpska_home!\tmp\!video_file_name!!video_ext!" 
 del "!fpska_home!\tmp\*.txt"
 del "!fpska_home!\tmp\*.h264"
@@ -153,6 +167,14 @@ cd "!fpska_home!"
 
 )
 
+if %errorlevel%==0 (
+	echo Звуковая дорожка извлечена успешно
+	echo.
+) else (
+	echo Ошибка извлечения звуковой дорожки
+	pause
+	exit
+)
 
 rem =================================================
 
@@ -222,29 +244,32 @@ echo --------------------------------------------------------
 EXIT /B 0
 
 :Check_Install
+
 set not_installed=0
+
 if not exist "!fpska_home!eac3to\eac3to.exe" (
-	echo eac3to not installed, please run setup.bat as Administrator
+	echo eac3to не установлена, запустите setup.bat от Administrator
 	set not_installed=1
 )
 
 if not exist "!fpska_home!ffmpeg\ffmpeg.exe" (
-	echo ffmpeg not installed, please run setup.bat as Administrator
+	echo ffmpeg не установлен, запустите setup.bat от Administrator
 	set not_installed=1
 )
 
 if not exist "!fpska_home!mkvtoolnix\mkvmerge.exe" (
-	echo mkvtoolnix not installed, please run setup.bat as Administrator
+	echo mkvtoolnix не установлены, запустите setup.bat от Administrator
 	set not_installed=1
 )
 
 if not exist "!fpska_home!svpflow\svpflow1.dll" (
-	echo svpflow not installed, please run setup.bat as Administrator
+	echo svpflow не установлена, запустите setup.bat от Administrator
 	set not_installed=1
 )
 
 if "!not_installed!"=="1" (
-pause
-exit
+	pause
+	exit
 )
+
 EXIT /B 0
